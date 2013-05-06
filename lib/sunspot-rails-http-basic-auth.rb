3563a8ba9fabda9a1fce71986dd2f1a5b9135666
Daniel Honig <Daniel.Honig@directnorth.biz>
2013-05-06 16:31:09 -0400

    changes to make everything work with latest sunspot

--- lib/sunspot-rails-http-basic-auth.rb
+++ lib/sunspot-rails-http-basic-auth.rb
@@ -1,10 +1,10 @@
 require 'rsolr'
 require 'sunspot/rails'
 
-require 'sunspot-rails-http-basic-auth/rsolr/connection/net_http'
+#require 'sunspot-rails-http-basic-auth/rsolr/client'
 require 'sunspot-rails-http-basic-auth/sunspot/rails'
 require 'sunspot-rails-http-basic-auth/sunspot/rails/configuration'
 
-if defined?(Rails) && Rails::VERSION::MAJOR == 2
-  Sunspot.session = Sunspot::Rails.build_session
-end
+#if defined?(Rails) && Rails::VERSION::MAJOR == 3
+  #Sunspot.session = Sunspot::Rails.build_session
+#end
